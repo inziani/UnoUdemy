ad3291ba04c3faf54b3a800970f12cf9102b1d4b
“inziani” <ValentineRobai.Inziani@gmail.com>
2021-03-23 10:03:58 +0300

    Project set up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'unoUdemy';
+  serverElements = [{type: 'server', name: 'TestServer', content: 'Just a test'}];
 }
