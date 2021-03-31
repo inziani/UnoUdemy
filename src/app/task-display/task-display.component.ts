d5c0eb6b7e4616163c6bea59d94b6b5042d4d833
“inziani” <ValentineRobai.Inziani@gmail.com>
2021-03-31 17:42:48 +0300

    reformat task list with edit and delete buttons

--- src/app/task-display/task-display.component.ts
+++ src/app/task-display/task-display.component.ts
@@ -6,11 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-display.component.css']
 })
 export class TaskDisplayComponent implements OnInit {
+  color = ''
 
   taskStatus = ['Created', 'Work in progress', 'Closed'];
   selectedStatus = '';
 
-  listDisplay = [{taskId: 'taskId', taskDesc: 'taskDesc', taskDetails: 'taskDetails', taskComments: 'taskComments'}];
+  listDisplay = [{date: 'date', taskId: 'taskId', taskDesc: 'taskDesc', taskDetails: 'taskDetails', taskComments: 'taskComments'}];
 
 
 
@@ -19,11 +20,22 @@ export class TaskDisplayComponent implements OnInit {
   ngOnInit(): void {
   }
 
+   ngOnChanges(){
+
+    //  if (this.taskStatus === 'Created'){
+    //    this.color = 'red';
+    //  }
+    //  else if ( this.taskStatus === 'Work in progress'){
+    //    this.color = 'amber';
+    //  }
+    //  else {
+    //    this.color = 'green';
+    //  }
+  }
+
   changeStatus(event: any){
     console.log(event.target.value);
-    this.listDisplay.push(event);
-
-
+    // this.listDisplay[1].taskComments = this.selectedStatus;
 
   }
 
--- src/app/task/task.ts
+++ src/app/task/task.ts
@@ -1,6 +1,7 @@
 export class Task {
 
   constructor(
+    public date: Date,
     public taskId: number,
     public taskDesc: string,
     public taskDetails: string )
